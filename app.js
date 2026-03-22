1d3ea8a26040f2ec230d47765123ef5e276052c9
Toni Daada <tonidaada@gmail.com>
2026-03-22 02:01:42 +0100

    Implementing user routes

--- app.js
+++ app.js
@@ -1,40 +1,44 @@
 const fs = require('fs');
 const express = require('express');
-
+const morgan = require('morgan');
 // Initializing express
 const app = express();
 
-// Middle ware
+// ExpressMiddle ware
+app.use(morgan('dev'));
 app.use(express.json());
 
-// // Creating the route
-// app.get('/', (req, res) => {
-//   res.status(200).json({
-//     message: 'Hello from the server side!',
-//     status: true,
-//   });
-// });
+// Creating our own middleware
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  next();
+});
 
-// app.post('/', (req, res) => {
-//   res.send('You can send to this endpoint');
-// });
-// Open a server
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
 );
 
-app.get('/api/v1/tours', (req, res) => {
+// 2) Route handlers
+
+const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours: tours,
     },
   });
-});
+};
 
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTourById = (req, res) => {
   console.log(req.params.id);
   const tour = tours.find((el) => el.id === Number(req.params.id));
 
@@ -52,13 +56,9 @@ app.get('/api/v1/tours/:id', (req, res) => {
       tour,
     },
   });
-});
+};
 
-/* This code snippet is defining a POST route in the Express.js server. When a POST request is made to
-the '/api/v1/tours' endpoint, the server will execute the callback function provided. Here's a
-breakdown of what the code inside the callback function is doing: */
-
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
   console.log(req.body);
 
   const newID = tours[tours.length - 1].id + 1;
@@ -85,9 +85,9 @@ app.post('/api/v1/tours', (req, res) => {
   );
 
   // res.send('Done');
-});
+};
 
-app.patch('/api/v1/tours/:id', (req, res) => {
+const updateTour = (req, res) => {
   // const tour = tours.find((el) => el.id === Number(req.params.id));x
 
   if (req.params.id * 1 > tours.length)
@@ -101,9 +101,9 @@ app.patch('/api/v1/tours/:id', (req, res) => {
       tour: '<Updated tour here...>',
     },
   });
-});
+};
 
-app.delete('/api/v1/tours/:id', (req, res) => {
+const deleteTour = (req, res) => {
   // const tour = tours.find((el) => el.id === Number(req.params.id));x
 
   if (req.params.id * 1 > tours.length)
@@ -115,7 +115,68 @@ app.delete('/api/v1/tours/:id', (req, res) => {
     status: 'success',
     data: null,
   });
-});
+};
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'err',
+    message: 'This route is not yet defined',
+  });
+};
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'err',
+    message: 'This route is not yet defined',
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'err',
+    message: 'This route is not yet defined',
+  });
+};
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'err',
+    message: 'This route is not yet defined',
+  });
+};
+
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'err',
+    message: 'This route is not yet defined',
+  });
+};
+
+// app.get('/api/v1/tours', getAllTours);
+
+// app.get('/api/v1/tours/:id', getTourById);
+
+// app.post('/api/v1/tours', createTour);
+
+// app.patch('/api/v1/tours/:id', updateTour);
+
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+// Our routes but here I am chaining the routes,
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+
+app
+  .route('/api/v1.tours/:id')
+  .get(getTourById)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+app.route('/api/v1/users').get(getAllUsers).post(createUser);
+
+app
+  .route('/api/v1/users/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 /* This code snippet is setting up a server using Express.js. It is defining a port number 3000 using
 `const port = 3000` and then starting the server to listen on that port using
