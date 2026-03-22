5d8545d65360d5d2fc2f332d867a5ac4e3739c65
Toni Daada <tonidaada@gmail.com>
2026-03-22 15:55:55 +0100

    serving static files and environment variables

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // ExpressMiddle ware
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
+// Serving static files
+app.use(express.static(`${__dirname}/public`));
 
 // Creating our own middleware
 app.use((req, res, next) => {
